9064ed8a6247f8a926d267b7991b5a637d6dc946
Dalí Freire <dalifreire@gmail.com>
2015-12-10 16:42:52 -0200

    Ajustes na configuração do logback.

--- jerimum-parent/jerimum-framework/src/main/java/br/com/jerimum/fw/logging/LoggerConfigurator.java
+++ jerimum-parent/jerimum-framework/src/main/java/br/com/jerimum/fw/logging/LoggerConfigurator.java
@@ -1,20 +1,22 @@
 package br.com.jerimum.fw.logging;
 
+import java.io.File;
+
 import org.slf4j.LoggerFactory;
 
+import ch.qos.logback.classic.BasicConfigurator;
 import ch.qos.logback.classic.LoggerContext;
 import ch.qos.logback.classic.joran.JoranConfigurator;
-import ch.qos.logback.core.joran.spi.JoranException;
 import ch.qos.logback.core.util.StatusPrinter;
 
 public class LoggerConfigurator {
 
     private String logbackConfigurationFile;
-    
+
     public LoggerConfigurator(String logbackConfigurationFile) {
         this.logbackConfigurationFile = logbackConfigurationFile;
     }
-    
+
     public void configure() {
 
         // assume SLF4J is bound to logback in the current environment
@@ -23,12 +25,17 @@ public class LoggerConfigurator {
         try {
             JoranConfigurator configurator = new JoranConfigurator();
             configurator.setContext(context);
-            // Call context.reset() to clear any previous configuration, e.g. default
-            // configuration. For multi-step configuration, omit calling context.reset().
+            /*
+             * Call context.reset() to clear any previous configuration, e.g. default configuration.
+             * For multi-step configuration, omit calling context.reset().
+             */
             context.reset();
-            configurator.doConfigure(logbackConfigurationFile);
-        } catch (JoranException je) {
-            // StatusPrinter will handle this
+            configurator.doConfigure(new File(logbackConfigurationFile));
+
+        } catch (Exception e) {
+            BasicConfigurator.configure(context);
+            LoggerUtils.logError(this.getClass(),
+                "Falha oa configurar logback, utilizando configuracao BasicConfigurator...", e);
         }
         StatusPrinter.printInCaseOfErrorsOrWarnings(context);
     }
